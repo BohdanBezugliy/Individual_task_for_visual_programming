4677991300170ab8a52db56054ee96c109d29a77
Богдан Безуглый <winzar06031977@gmail.com>
2025-06-02 00:04:55 +0300

    Add project files.

---
+++ Individual_task_for_visual_programming/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Individual_task_for_visual_programming;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
